6480254bd481df61fe896436ab1238386e22589a
Gaetano Smario <gaetano.smario@liparipeople.com>
2024-01-25 21:30:57 +0100

    updated index

---
+++ scripts.js
@@ -0,0 +1,71 @@
+const calendar = document.getElementById("calendar");
+const today = new Date();
+const days = ["Domenica", "Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato"];
+const months = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"];
+const games = [
+ {
+ date: new Date("2024-01-27"),
+ opponent: "RIBERA KNIGHTS"
+ },
+ {
+ date: new Date("2024-02-04"),
+ opponent: "A.S.D. INVICTA 93CENTO"
+ },
+ {
+ date: new Date("2024-02-11"),
+ opponent: "POL. DIL. IDEAL GELA"
+ },
+ {
+ date: new Date("2024-02-25"),
+ opponent: "VIRTUS SOMMATINO"
+ },
+ {
+ date: new Date("2024-03-03"),
+ opponent: "RIBERA KNIGHTS"
+ },
+ {
+ date: new Date("2024-03-11"),
+ opponent: "A.S.D. INVICTA 93CENTO"
+ },
+ {
+ date: new Date("2024-02-18"),
+ opponent: "A.S.D. C.U.S.N."
+ }
+];
+
+const nextGame = () => {
+ let nextGame = null;
+ for (let i = 0; i < games.length; i++) {
+ if (games[i].date > today) {
+ nextGame = games[i];
+ break;
+ }
+ }
+ if (nextGame) {
+ const daysUntil = Math.ceil((nextGame.date - today) / (1000 * 60 * 60 * 24));
+ return `La prossima partita è il ${nextGame.date.getDate()} ${months[nextGame.date.getMonth()]} contro ${nextGame.opponent} tra ${daysUntil} giorni.`;
+ } else {
+ return "Non ci sono partite in programma.";
+ }
+};
+
+calendar.innerHTML = nextGame();
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const navMenuBtn = document.getElementById('nav-menu-btn');
+  const navMenu = document.getElementById('nav-menu');
+
+  navMenuBtn.addEventListener('click', function () {
+    navMenu.style.display = (navMenu.style.display === 'block') ? 'none' : 'block';
+  });
+
+  window.addEventListener('resize', function () {
+    if (window.innerWidth >= 768) {
+      navMenu.style.display = 'block';
+    } else {
+      navMenu.style.display = 'none';
+    }
+  });
+});
+
